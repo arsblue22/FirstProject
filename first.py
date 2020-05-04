64738542fbb9d97d448f2922f9a18edafd31bced
Amberlynn <arsblue22@gmail.com>
2020-05-03 21:34:38 -0400

    fruitList

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("Mrs.Amberlynn Shorrow")
+name = "Mrs.Amberlynn Shorrow"
+print(name)
+print(name[0:13])
+print(len(name))
+
+fruit = ["apple","orange","banana"]
+print("My list has: ")
+for x in fruit:
+    if x == 'orange':
+        print("grape")
+    else:
+        print(x)
